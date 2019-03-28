7302985a9235d0d5dae79e84e6fdd6fe739487e1
Arka Pal <arkapal.pal@gmail.com>
2019-03-29 00:58:47 +0530

    updated the records

--- simulator.h
+++ simulator.h
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define add_cycles 2
+#define sub_cycles 2
+#define mul_cycles 10
+#define div_cycles 40
 
 enum opcode {add,sub,mul,div};
 struct InstrRecord
@@ -10,6 +14,17 @@ struct InstrRecord
   int src1,src2;     // source registers
 };
 
+struct Broadcast{
+  int rsTag;
+  int val;
+};
+
+struct ExecutionUnit{
+  int rsTag;
+  int val;
+  int writebackCycle;
+};
+
 struct ReservationStation
 {
   bool busy;
@@ -17,15 +32,16 @@ struct ReservationStation
   int Qk;
   int Vj;
   int Vk;
-  int lat;
+  int latency;
   int op;
   int result;
   bool resultReady;
   int instNum;
-  int ISSUE_Lat;
-  int WRITEBACK_Lat;
+  int disp;                   // 0: not dispatched, 1: dispatched.
+  int writebackCycle;         // cycle it will attempt writeback
 };
 
 queue<InstrRecord> instructionQ;
 vector<int> registerFile(8);    // Register File having 8 entries 0 to 7
 vector<int> rat(8,-1);          // if RAT entry is -1, it means the RAT points to the register in the register file and not any RS
+ExecutionUnit addUnit, mulUnit;
