396d233dd4282918b064d4f71d17f246d6098bd4
Arka Pal <arkapal.pal@gmail.com>
2019-03-29 14:01:05 +0530

    added dispatch to add unit

--- simulator.cpp
+++ simulator.cpp
@@ -1,7 +1,49 @@
 #include "simulator.h"
 
 
-s
+void  dispatch_to_add_unit(int cycle)
+{
+  if(cycle>=addUnit.writebackCycle)                 // Add Unit not currently executing something
+  {
+    bool rsReady=false;
+    for(int i=0;i<=2;i++)                          // poll the RS in decreasing priority to check readiness
+      if(RS[i].busy && RS[i].Qj!=-1 && RS[i].Qk!=-1)
+      {
+        rsReady=true;
+        break;
+      }
+      if(rsReady)                                  // Reservation Station ready to go
+      {
+        RS[i].dispatch=1;
+        RS[i].writebackCycle=cycle+add_latency;     // cycle the instruction will attempt a writeback
+        addUnit.busy=true;
+        addUnit.writebackCycle=cycle+add_latency;   // cycle the unit will attempt a writeback
+      }
+  }
+
+}
+
+void  dispatch_to_mul_unit(int cycle)
+{
+  if(cycle>=mulUnit.writebackCycle)                 // Add Unit not currently executing something
+  {
+    bool rsReady=false;
+    for(int i=3;i<=4;i++)                          // poll the RS in decreasing priority to check readiness
+      if(RS[i].busy && RS[i].Qj!=-1 && RS[i].Qk!=-1)
+      {
+        rsReady=true;
+        break;
+      }
+      if(rsReady)                                  // Reservation Station ready to go
+      {
+        RS[i].dispatch=1;
+        RS[i].writebackCycle=cycle+(RS[i].opcode==2?mul_latency:div_latency);     // cycle the instruction will attempt a writeback
+        mulUnit.busy=true;
+        mulUnit.writebackCycle=cycle+(RS[i].opcode==2?mul_latency:div_latency);   // cycle the unit will attempt a writeback
+      }
+  }
+
+}
 
 void dispatch(int cycle)
 {
