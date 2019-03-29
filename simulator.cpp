275ee781a6468eb27d5f89f3a64b647237c6a60d
Arka Pal <arkapal.pal@gmail.com>
2019-03-29 15:23:31 +0530

    added function to issue instructions

--- simulator.cpp
+++ simulator.cpp
@@ -18,7 +18,7 @@ void  dispatch_to_add_unit(int cycle)
     bool rsReady=false;
     int i;
     for(i=0;i<=2;i++)                          // poll the RS in decreasing priority to check readiness
-      if(RS[i].busy && RS[i].Qj!=-1 && RS[i].Qk!=-1)
+      if(RS[i].busy && RS[i].Qj==-1 && RS[i].Qk==-1)
       {
         rsReady=true;
         break;
@@ -42,7 +42,7 @@ void  dispatch_to_mul_unit(int cycle)
     bool rsReady=false;
     int i;
     for(i=3;i<=4;i++)                          // poll the RS in decreasing priority to check readiness
-      if(RS[i].busy && RS[i].Qj!=-1 && RS[i].Qk!=-1)
+      if(RS[i].busy && RS[i].Qj==-1 && RS[i].Qk==-1)
       {
         rsReady=true;
         break;
@@ -65,13 +65,74 @@ void dispatch(int cycle)
   dispatch_to_mul_unit(cycle);
 }
 
+SameCycleUpdate issue(int cycle)
+{
+  SameCycleUpdate update;
+  update.needsUpdate=false;
+  if(!instructionQ.empty())
+  {
+    InstrRecord instr=instructionQ.front();
+    bool rsAvailable=false;int i=-1;
+    if(instr.opcode<=1)
+    {
+      for(i=0;i<=2;i++)
+        if(RS[i].busy==false)
+        {
+          rsAvailable=true;
+          break;
+        }
+    }
+    else
+    {
+      for(i=3;i<=4;i++)
+        if(RS[i].busy==false)
+        {
+          rsAvailable=true;
+          break;
+        }
+    }
+    if(rsAvailable)
+    {
+        // issue the instruction from the instr Q to the RS i
+        instructionQ.pop();
+        RS[i].busy=true;
+        RS[i].opcode=isntr.opcode;
+        RS[i].dispatch=0;
+        int operand1=instr.src1,operand2=instr.src2;
+        if(RAT[operand1]==-1)                   // RAT points to registers
+        {
+          RS[i].Vj=registerFile[operand1]      // get value from register file
+          RS[i].Qj=-1;                          // not waiting for anything
+        }
+        else
+        {
+          RS[i].Qj=registerFile[operand1]      // tag of first RS its waiting for
+        }
+        if(RAT[operand2]==-1)                   // RAT points to registers
+        {
+          RS[i].Vk=registerFile[operand2]      // get value from register file
+          RS[i].Qk=-1;                         // not waiting for anything
+        }
+        else
+        {
+          RS[i].Qk=registerFile[operand2]      // tag of second RS its waiting for
+        }
+        update.rsTag=i;
+        update.needsUpdate=true;
+        update.opRegister=instr.dst;
+    }
+
+  }
+  return update;
+}
+
 int main()
 {
   int cycle=1,targetCycle=100;
   while(cycle<=targetCycle)
   {
     dispatch(cycle);
-    //issue();
+    SameCycleUpdate update=issue();
   //  broadcast();
     cycle++;
   }
